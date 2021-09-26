42a4f5bd1a5e3d089f5302568f3ce675e5914562
VIB2020 <vibhavasu2018@gmail.com>
2021-09-26 20:03:26 +0530

    2609.2003

--- Assignment 1/postfix.py
+++ Assignment 1/postfix.py
@@ -1,5 +1,5 @@
 from oop1 import Stacks
-expression1 = ['2','23','+','11','12','13','-','*','-']
+expression1 = ['2','23','+','11','12','13','-','*','-', '35']
 
 def postfixexp(expression1):
     s = Stacks()
@@ -22,8 +22,23 @@ def postfixexp(expression1):
             a = s.Pop()
             b = s.Pop()
             s.Push(b/a)
+    return s.Top()
+
+def validate(expression1):
+    j=0
+    k=0
+    for i in expression1:
+        if i.isdigit() == True:
+            j = j+1
+        elif i== '+' or i== '-' or i== '*' or i== '/':
+            k = k+1
         else:
             print(f"Invalid character at {s.array.index(x)}")
-            return 3  
-    return s.Top()
-print(f"{postfixexp(expression1)}")
+            return 3
+
+    if j == k+1:
+        return postfixexp(expression1)
+    else:
+        print("Invalid Expression")
+        return 4
+print(f"{validate(expression1)}")
